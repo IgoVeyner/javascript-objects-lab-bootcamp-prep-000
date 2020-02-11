635cd466c827142715f5814159374094992c473e
Igor Veyner <igorveyner95@gmail.com>
2020-02-11 22:00:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object;
+  return Object.assign({}, object, { [key]: })
 }
