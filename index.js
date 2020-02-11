f9b26a9dab3789c252b1d40c0dbdd571cd2b15bd
Igor Veyner <igorveyner95@gmail.com>
2020-02-11 21:59:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object;
 }
 
-function destructivelyDeleteFromObjectByKey(object, key) {
+function deleteFromObjectByKey(object, key) {
   delete object.key;
   return object;
 }
